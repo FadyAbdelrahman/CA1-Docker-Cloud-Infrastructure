8823c8c0bd022d30e32e32f9ee588e36a28b5b1a
Fady Abdelrahman <fadyabdelrahman786@gmail.com>
2026-03-25 07:46:09 +0000

    Initial commit - Fady 20053593

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,8 +53,11 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+            // Old text: <p className="text-center"> You have no todo items yet! Add one above! </p>
+            // New text updated by Fady Abdelrahman
+              <p className="text-center">Ready to stay organized? Add your first todo item now.</p>
             )}
+
             {items.map(item => (
                 <ItemDisplay
                     item={item}
